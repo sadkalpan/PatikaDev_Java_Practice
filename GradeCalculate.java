d374a9a387bb5d545f383881d9ca6bf6a7477c57
Sadık ALPAN <59167960+sadkalpan@users.noreply.github.com>
2022-05-13 12:37:40 +0300

    Add files via upload

---
+++ GradeCalculate.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class GradeCalculate {
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+        int mathematic, physic, chemistry, turkish, history, music;
+        float average_grade;
+        String StringResult;
+
+        System.out.println("enter your mathematic grade: ");
+        mathematic = inp.nextInt();
+
+        System.out.println("enter your physic grade: ");
+        physic = inp.nextInt();
+
+        System.out.println("enter your chemistry grade: ");
+        chemistry = inp.nextInt();
+
+        System.out.println("enter your turkish grade: ");
+        turkish = inp.nextInt();
+
+        System.out.println("enter your history grade: ");
+        history = inp.nextInt();
+
+        System.out.println("enter your music grade: ");
+        music = inp.nextInt();
+
+        average_grade = (mathematic+physic+chemistry+turkish+history+music)/6;
+        System.out.println("Avarega grade is the: " + average_grade);
+
+        StringResult =  average_grade > 60 ? "sinifi gecti" : "sinifta kaldi54";
+        System.out.println(StringResult);
+    }
+}
