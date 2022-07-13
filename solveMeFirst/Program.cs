c04feb63e63b548b76347b1634a49bd541a4816e
Sadık ALPAN <59167960+sadkalpan@users.noreply.github.com>
2022-07-13 12:08:49 +0300

    Add files via upload

---
+++ solveMeFirst/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace solveMeFirst
+{
+    internal class Program
+    {
+        static int solveMeFirst(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sum = solveMeFirst(a, b);
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ solveMeFirst/ProgramBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace solveMeFirst
+{
+    internal class ProgramBase
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a + b);
+        }
+    }
+}
